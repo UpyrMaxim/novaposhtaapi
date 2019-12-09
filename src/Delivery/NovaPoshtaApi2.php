809c72abbbc0317ffea0cb12458b6bb25d88c1e2
Max Upyr <max.ypurgmail.com>
2019-12-09 14:34:18 +0000

    simple changes

--- src/Delivery/NovaPoshtaApi2.php
+++ src/Delivery/NovaPoshtaApi2.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace LisDev\Delivery;
+namespace Delivery;
 
 /**
  * Nova Poshta API Class.
@@ -493,7 +493,7 @@ class NovaPoshtaApi2
     public function getArea($findByString = '', $ref = '')
     {
         // Load areas list from file
-        empty($this->areas) and $this->areas = include dirname(__FILE__).'/NovaPoshtaApi2Areas.php';
+        empty($this->areas) and $this->areas = include dirname(__FILE__) . '/NovaPoshtaApi2Areas.php';
         $data = $this->findArea($this->areas, $findByString, $ref);
         // Error
         empty($data) and $error = 'Area was not found';
--- tests/NovaPoshtaApi2Test.php
+++ tests/NovaPoshtaApi2Test.php
@@ -2,7 +2,7 @@
 
 namespace LisDev\Tests;
 
-use LisDev\Delivery\NovaPoshtaApi2;
+use Delivery\NovaPoshtaApi2;
 
 /**
  * phpUnit test class.
